1f6741f029d97b52ec2055876fbb82799be2c95e
rn10rd9 <RN10RD9@wpa-5-1135.cc.umanitoba.ca>
2016-12-01 14:57:55 -0600

    Working Multiplexed
    This code contains multiplexes the data from both sensors succesfully

--- sample/sample.ino
+++ sample/sample.ino
@@ -12,11 +12,33 @@ extern "C" {
 #define BNO055_SAMPLERATE_DELAY_MS (1000)
 
 Adafruit_BNO055 bno1 = Adafruit_BNO055(55);
+Adafruit_BNO055 bno2 = Adafruit_BNO055();
 
+void multiplexSelector()
+{
+  for (uint8_t t = 0; t < 8; t++)
+  {
+    tcaselect(t);
+    Serial.print("TCA Port #"); Serial.println(t);
+
+    for (uint8_t addr = 0; addr <= 127; addr++)
+    {
+      if (addr == TCAADDR) continue;
+
+      uint8_t data;
+      if (! twi_writeTo(addr, &data, 0, 1, 1))
+      {
+        Serial.print("Found I2C 0x");  Serial.println(addr, HEX);
+
+      }
+    }
+  }
+
+}
 
 void displaySensorDetails(Adafruit_BNO055*bno)
 {
-  sensor_t sensor; 
+  sensor_t sensor;
   bno->getSensor(&sensor);
   Serial.println("------------------------------------");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
@@ -37,6 +59,25 @@ void tcaselect(uint8_t i) {
   Wire.endTransmission();
 }
 
+void displaySensorStatus(void)
+{
+  /* Get the system status values (mostly for debugging purposes) */
+  uint8_t system_status, self_test_results, system_error;
+  system_status = self_test_results = system_error = 0;
+  bno1.getSystemStatus(&system_status, &self_test_results, &system_error);
+
+  /* Display the results in the Serial Monitor */
+  Serial.println("");
+  Serial.print("System Status: 0x");
+  Serial.println(system_status, HEX);
+  Serial.print("Self Test:     0x");
+  Serial.println(self_test_results, HEX);
+  Serial.print("System Error:  0x");
+  Serial.println(system_error, HEX);
+  Serial.println("");
+  delay(500);
+}
+
 
 // standard Arduino setup()
 void setup()
@@ -45,48 +86,41 @@ void setup()
   delay(1000);
 
   Wire.begin();
-  
+
   Serial.begin(115200);
   Serial.println("Orientation Sensor Test"); Serial.println("");
   /* Initialise the 1st sensor */
-
-  for (uint8_t t=0; t<8; t++)
+  tcaselect(0);
+  if (!bno1.begin())
   {
-    tcaselect(t);
-    Serial.print("TCA Port #"); Serial.println(t);
-
-    for (uint8_t addr =0; addr<=127; addr++)
-    {
-      if (addr == TCAADDR) continue;
-
-      uint8_t data;
-      if (! twi_writeTo(addr, &data, 0, 1,1))
-      {
-        Serial.print("Found Sensor") ;
-        if (!bno1.begin())
-        {
-          /* There was a problem detecting the HMC5883 ... check your connections */
-          Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-          while (1);
-        } 
-        displaySensorDetails(&bno1);
-      }
-    }
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while (1);
   }
- 
 
- 
+  tcaselect(0);
+  if (!bno2.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    while (1);
+  }
 
+
+  //displaySensorDetails(&bno1);
+  //displaySensorStatus();
 }
 
 void loop()
 {
   /* Get a new sensor event */
   sensors_event_t event;
+
+  tcaselect(0);
   bno1.getEvent(&event);
-            
+
   /* The processing sketch expects data as roll, pitch, heading */
-  Serial.print(F("Orientation 1: "));
+  Serial.print(F("\nOrientation 1: "));
   Serial.print((float)event.orientation.x);
   Serial.print(F(" "));
   Serial.print((float)event.orientation.y);
@@ -94,25 +128,25 @@ void loop()
   Serial.print((float)event.orientation.z);
   Serial.println(F(""));
 
-  /* Also send calibration data for each sensor. 
-  uint8_t sys, gyro, accel, mag = 0;
-  bno1.getCalibration(&sys, &gyro, &accel, &mag);
-  Serial.print(F("Calibration: "));
-  Serial.print(sys, DEC);
-  Serial.print(F(" "));
-  Serial.print(gyro, DEC);
-  Serial.print(F(" "));
-  Serial.print(accel, DEC);
-  Serial.print(F(" "));
-  Serial.println(mag, DEC);
-   
+  /* Also send calibration data for each sensor.
+    uint8_t sys, gyro, accel, mag = 0;
+    bno1.getCalibration(&sys, &gyro, &accel, &mag);
+    Serial.print(F("Calibration: "));
+    Serial.print(sys, DEC);
+    Serial.print(F(" "));
+    Serial.print(gyro, DEC);
+    Serial.print(F(" "));
+    Serial.print(accel, DEC);
+    Serial.print(F(" "));
+    Serial.println(mag, DEC);*/
+
   delay(BNO055_SAMPLERATE_DELAY_MS);
+  tcaselect(2);
+  bno2.getEvent(&event);
 
-   bno2.getEvent(&event);
 
-
-  The processing sketch expects data as roll, pitch, heading 
-  Serial.print(F("Orientation 2: "));
+//  The processing sketch expects data as roll, pitch, heading
+  Serial.print(F("\nOrientation 2: "));
   Serial.print((float)event.orientation.x);
   Serial.print(F(" "));
   Serial.print((float)event.orientation.y);
@@ -120,20 +154,20 @@ void loop()
   Serial.print((float)event.orientation.z);
   Serial.println(F(""));
 
-   Also send calibration data for each sensor. 
+  /* Also send calibration data for each sensor.
 
-  bno2.getCalibration(&sys, &gyro, &accel, &mag);
-  Serial.print(F("Calibration 2: "));
-  Serial.print(sys, DEC);
-  Serial.print(F(" "));
-  Serial.print(gyro, DEC);
-  Serial.print(F(" "));
-  Serial.print(accel, DEC);
-  Serial.print(F(" "));
-  Serial.println(mag, DEC);*/
-   
+    bno2.getCalibration(&sys, &gyro, &accel, &mag);
+    Serial.print(F("Calibration 2: "));
+    Serial.print(sys, DEC);
+    Serial.print(F(" "));
+    Serial.print(gyro, DEC);
+    Serial.print(F(" "));
+    Serial.print(accel, DEC);
+    Serial.print(F(" "));
+    Serial.println(mag, DEC);*/
+
   delay(BNO055_SAMPLERATE_DELAY_MS);
- 
+
 
 
 }
